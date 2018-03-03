6e5d0d16fee86b32f3dd9f2ab3b17ef50e2f7535
Janne Koschinski <janne@kuschku.de>
2018-03-03 15:58:36 +0100

    Fixes timeout options

--- database/Config.php
+++ database/Config.php
@@ -27,12 +27,13 @@ class Config
     public static function createFromGlobals()
     {
         $options = [];
-        if (defined(qrs_db_option_tsqueryfunction) && null !== qrs_db_option_tsqueryfunction)
+        if (defined('qrs_db_option_tsqueryfunction') && (null !== qrs_db_option_tsqueryfunction)) {
             $options['tsqueryfunction'] = qrs_db_option_tsqueryfunction;
+        }
 
-        $options['timeout'] = (defined(qrs_db_option_timeout) && null !== qrs_db_option_timeout) ? qrs_db_option_timeout : 5000;
+        $options['timeout'] = (defined('qrs_db_option_timeout') && (null !== qrs_db_option_timeout)) ? qrs_db_option_timeout : 5000;
 
-        if (defined(qrs_db_connector) && null !== qrs_db_connector)
+        if (defined('qrs_db_connector') && null !== qrs_db_connector)
             return new Config(qrs_path_prefix, qrs_db_connector, qrs_db_user, qrs_db_pass, qrs_backend, $options);
         else
             return new Config(qrs_path_prefix, 'pgsql:host=' . qrs_db_host . ';port=' . qrs_db_port . ';dbname=' . qrs_db_name . '', qrs_db_user, qrs_db_pass, qrs_backend, $options);
